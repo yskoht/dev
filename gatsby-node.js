fd3a93c95024a002dad229720874b6890c9c1e99
yskoht <ysk.oht@gmail.com>
2022-07-09 18:02:04 +0900

    mod gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,44 @@
-exports.createPages = async ({ actions }) => {
+const { createFilePath } = require("gatsby-source-filesystem")
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({ node, getNode, trailingSlash: false })
+
+    createNodeField({
+      name: "slug",
+      node,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.tsx"),
-    context: {},
-    defer: true,
+
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        nodes {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.nodes.forEach(node => {
+    const { slug } = node.fields
+
+    createPage({
+      path: slug,
+      component: require.resolve("./src/templates/using-dsg.tsx"),
+      context: {
+        slug,
+      },
+      defer: true,
+    })
   })
 }
